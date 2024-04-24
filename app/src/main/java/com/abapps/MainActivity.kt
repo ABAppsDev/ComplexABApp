6fba3c82692cf2c28829f3a1d19f36b818ed0e64
Mostafa Mohamed <mostafamansour163@yahoo.com>
2024-04-24 15:19:12 +0200

    add home screen to first open in launch application

--- app/src/main/java/com/abapps/MainActivity.kt
+++ app/src/main/java/com/abapps/MainActivity.kt
@@ -10,6 +10,8 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import cafe.adriel.voyager.navigator.Navigator
+import com.abapps.presentation.home.screens.HomeScreen
 import com.abapps.ui.theme.ABAppsTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,30 +19,8 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ABAppsTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+                Navigator(screen = HomeScreen)
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ABAppsTheme {
-        Greeting("Android")
     }
 }
